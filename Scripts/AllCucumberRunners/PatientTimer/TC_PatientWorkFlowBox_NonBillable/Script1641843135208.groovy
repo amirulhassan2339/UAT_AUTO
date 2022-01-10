26d5bede637ac40840bf307f1b41d5b965622b11
Amir <Hafiz.amir@sltn.net>
2022-01-11 00:47:43 +0500

    UAT Updated

---
+++ Scripts/AllCucumberRunners/PatientTimer/TC_PatientWorkFlowBox_NonBillable/Script1641843135208.groovy
@@ -0,0 +1,7 @@
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+
+import patientTimer.CR_PatientWorkFlowBoxBillable
+import patientTimer.CR_PatientWorkFlowBoxNonBillable
+
+
+CucumberKW.runWithCucumberRunner(CR_PatientWorkFlowBoxNonBillable.class)
